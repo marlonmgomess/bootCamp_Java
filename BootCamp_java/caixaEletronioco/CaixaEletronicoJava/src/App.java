63798f3b1958ae852e6a1bffdcc326cadd5a3aa4
marlonmgomess <marlonmgomess@hotmail.com>
2025-04-04 20:37:29 -0300

    colocando linhas abaixo dos menus

--- BootCamp_java/caixaEletronioco/CaixaEletronicoJava/src/App.java
+++ BootCamp_java/caixaEletronioco/CaixaEletronicoJava/src/App.java
@@ -10,14 +10,38 @@ public class App {
         boolean parar = false;
 
         do{
+            menu();
+            int opcao = scanner.nextInt();
+            switch(opcao){
+                case 1:
+
+                break;
+
+                case 2:
+
+                break;
+
+                case 3:
+
+                System.out.println("Saindo do Programa");
+                parar = true;
+            }
 
         }while(!parar);
         
     }
     public static void menu(){
         System.out.println("Caixa Eletronico JAVA 2022");
+        printLinhas();
         System.out.println("[1] Entrar na Connta");
+        printLinhas();
         System.out.println("[2] Criar Conta");
+        printLinhas();
         System.out.println("[3] Sair (Encerrar o Programa)");
+        printLinhas();
+    }
+
+    public static void printLinhas(){
+        System.out.println("------------------------------------");
     }
 }
